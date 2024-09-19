a03fcc85c4411a526033235fe8830a77b2e32f47
Johnni5 <johnni5@proton.me>
2024-09-19 20:32:41 +0200

    "new Fb-Gg account, implemented access and gg-login"

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,38 @@
+'use client'
+// import '../styles/globals.css'
+// import Button from '@mui/material/Button';
+// import useSignInWithGoogle from 'react-firebase-hooks/auth';
+// import auth from '../utils/firebase';
+
+// export default function Login() {
+//   const [ signInWithGoogle ] = useSignInWithGoogle(auth);
+//   return (
+//     <div className="app">
+//       <div className="login">
+//         <div className="login__background" />
+
+//         <div className="login__container">
+//           <img src="/logo.png" alt="logo" />
+//           <div className="login__text">
+//             <h1>Sign in to Epic Chat</h1>
+//           </div>
+//           <Button onClick={signInWithGoogle}>
+//             Sign in with Google
+//           </Button>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
 import '../styles/globals.css'
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import Button from '@mui/material/Button';
+import { auth } from '../utils/firebase';
 
 export default function Login() {
+  const [signInWithGoogle] = useSignInWithGoogle(auth);
+
   return (
     <div className="app">
       <div className="login">
@@ -12,7 +43,7 @@ export default function Login() {
           <div className="login__text">
             <h1>Sign in to Epic Chat</h1>
           </div>
-          <Button>
+          <Button onClick={() => signInWithGoogle()}>
             Sign in with Google
           </Button>
         </div>
